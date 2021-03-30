ed8d0edd8abc8ac2fb21e7d9b66d12992215876c
y.kohama <yk050696@gmail.com>
2021-03-30 15:50:13 +0900

    model edit

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -2,4 +2,13 @@ class Progress < ApplicationRecord
   has_one :content
 
   validates :study_date, presence: true
+  validates :today, :this_week, :this_month, :total_time, numericality: { greater_than: 0 }
+
+  def this_week
+    today * 7
+  end
+
+  def this_month
+    
+  end
 end
