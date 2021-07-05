6c0a4a778a45b8ef177ea0e4471b8938fe50d9c5
y.kohama <yk050696@gmail.com>
2021-07-05 17:58:33 +0900

    seed create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,62 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 3.times do |n|
-#   user = User.new(
-#     user_name: "テストユーザー#{n}",
-#     email: "test#{n}@gmail.com",
-#     password_digest: "password"
-#   )
-
-#   user.save!
-# end
-
+# 追加済　7/5時点
 2.times do |n|
-  book_mark = BookMark.new(
-    book_mark_name: "テストブックマーク#{n}",
-    tag: "ブックマークタグ#{n}",
-    book_mark_url: "https://qiita.com/"
+  user = User.new(
+    user_name: "テストユーザー#{n}",
+    email: "test#{n}@gmail.com",
+    password_digest: "password"
   )
 
-  book_mark.save!
+  user.save!
+end
+
+# 追加済　7/5時点
+# 2.times do |n|
+#   book_mark = BookMark.new(
+#     book_mark_name: "テストブックマーク#{n}",
+#     tag: "ブックマークタグ#{n}",
+#     book_mark_url: "https://qiita.com/"
+#   )
+
+#   book_mark.save!
+# end
+
+3.times do |n|
+  content = Content.new()
+
+  3.times do |o|
+    content.book_marks.build(
+      book_mark_name: "ブックマーク#{o}",
+      tag: "ブックマークタグ#{o}",
+      book_mark_url: "https://qiita.com/"
+    )
+  end
+
+  3.times do |p|
+    content.programming_languages.build(
+      programming_languages_name: "ruby",
+      logo: File.open('./app/assets/images/ruby.svg')
+    )
+  end
+
+  3.times do |q|
+    content.progresses.build(
+      study_date: "7/1",
+      today: "1:00",
+      this_week: "2:00",
+      this_month: "3:00",
+      total_time: "3:00"
+    )
+  end
+
+  3.times do |r|
+    content.tasks.build(
+      task_name: "タスク#{r}",
+      active: false
+    )
+  end
+
+  content.save!
 end
