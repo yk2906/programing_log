9bdf2fcfebbf726db0e65daccb63c8fcb5df4b97
y.kohama <yk050696@gmail.com>
2021-07-06 11:37:55 +0900

    fixed database

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :contents, dependent: :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :encrypted_password, presence: true, uniqueness: true, length: { minimum: 6 }
+end
--- db/migrate/20210311022801_create_contents.rb
+++ db/migrate/20210311022801_create_contents.rb
@@ -1,11 +1,9 @@
 class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
+      t.string :study_topics_title, null: false
       t.references :user, null: false, foreign_key: true
-      t.references :programming_language, null: false, foreign_key: true
-      t.references :task, foreign_key: true
-      t.references :book_mark, foreign_key: true
-
+     
       t.timestamps
     end
   end
